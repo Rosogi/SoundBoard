d182136d0c0a7cd23d9a1d9ac286a75334b320db
Rosogi <gsda123@yandex.ru>
2018-07-25 15:02:02 +0300

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,116 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.net.URL;
+
+public class Controller {
+/*
+    final URL resource = getClass().getResource("a.mp3");
+    final Media media = new Media(resource.toString());
+    final MediaPlayer mediaPlayer = new MediaPlayer(media);
+    mediaPlayer.play();
+ */
+    public void Button_1_P(MouseEvent mouseEvent) {
+        final URL resource = getClass().getResource("AIRHORN.mp3");
+        final Media media = new Media(resource.toString());
+        final MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
+    }
+
+    public void Button_1_R(MouseEvent mouseEvent) {
+
+    }
+
+    public void Button_2_P(MouseEvent mouseEvent) {
+        final URL resource = getClass().getResource("MOM GET THE CAMERA.mp3");
+        final Media media = new Media(resource.toString());
+        final MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
+    }
+
+    public void Button_2_R(MouseEvent mouseEvent) {
+
+    }
+
+    public void Button_3_P(MouseEvent mouseEvent) {
+        final URL resource = getClass().getResource("DAMN SON WHERED YOU FIND THIS.mp3");
+        final Media media = new Media(resource.toString());
+        final MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
+    }
+
+    public void Button_3_R(MouseEvent mouseEvent) {
+
+    }
+
+    public void Button_4_P(MouseEvent mouseEvent) {
+        final URL resource = getClass().getResource("GET NOSCOPED.mp3");
+        final Media media = new Media(resource.toString());
+        final MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
+    }
+
+    public void Button_4_R(MouseEvent mouseEvent) {
+
+    }
+
+    public void Button_5_P(MouseEvent mouseEvent) {
+        final URL resource = getClass().getResource("Oh Baby A Triple.mp3");
+        final Media media = new Media(resource.toString());
+        final MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
+    }
+
+    public void Button_5_R(MouseEvent mouseEvent) {
+
+    }
+
+    public void Button_6_P(MouseEvent mouseEvent) {
+        final URL resource = getClass().getResource("SMOKE WEEK EVERYDAY.mp3");
+        final Media media = new Media(resource.toString());
+        final MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
+    }
+
+    public void Button_6_R(MouseEvent mouseEvent) {
+
+    }
+
+    public void Button_7_P(MouseEvent mouseEvent) {
+        final URL resource = getClass().getResource("SPOOKY.mp3");
+        final Media media = new Media(resource.toString());
+        final MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
+    }
+
+    public void Button_7_R(MouseEvent mouseEvent) {
+
+    }
+
+    public void Button_8_P(MouseEvent mouseEvent) {
+        final URL resource = getClass().getResource("tactical nuke.mp3");
+        final Media media = new Media(resource.toString());
+        final MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
+    }
+
+    public void Button_8_R(MouseEvent mouseEvent) {
+
+    }
+
+    public void Button_9_P(MouseEvent mouseEvent) {
+        final URL resource = getClass().getResource("wow_.mp3");
+        final Media media = new Media(resource.toString());
+        final MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
+    }
+
+    public void Button_9_R(MouseEvent mouseEvent) {
+
+    }
+}
